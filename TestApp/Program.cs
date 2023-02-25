e969cc96ea94da1eafb89e27b1986213fa6ad334
AlikM11 <tamerlaailn7@gmail.com>
2023-02-25 17:38:17 +0400

    Add project files.

---
+++ TestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            User user = new User();
+            //user.Name = "Ilkin";
+            //Console.WriteLine(user.Name);
+            user.Any();
+        }
+    }
+}
---
+++ TestApp/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname{ get; set; }
+
+        public void Any()
+        {
+            Console.WriteLine("Any Method is works");
+        }
+    }
+}
